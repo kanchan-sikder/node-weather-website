eed9a5469e05949dff047b11c9f656fad7391625
kanchan.k.s <kanchansikder1408@gmail.com>
2021-09-02 21:44:24 +0530

    Changes in index

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+const messageFour = document.querySelector('#message-4')
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -9,7 +12,9 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
 
     messageOne.textContent = "Loading!!!"
-    messageTwo.textContent = ""
+    messageTwo.textContent =  ""
+    messageThree.textContent = ""
+    messageFour.textContent = ""
 
     fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
@@ -17,8 +22,10 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent =data.error
             }
             else{
-                messageOne.textContent =data.location 
-                messageTwo.textContent =data.forecast
+                messageOne.textContent = "LOCATION: "
+                messageTwo.textContent = data.location 
+                messageThree.textContent ="FORECAST: "
+                messageFour.textContent = data.forecast
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,20 @@
-const request= require("request")
+const request = require("request")
 
-const forecast =(latitude,longitude,callback)=>{
-const url='https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&appid=38e899ebd737adbc97156a0b1848ba5f&units=metric'
-request({url,json:true},(error,{body}=response)=>{
-    if(error){
-        callback("Not connected to the web server.Check internet connection.",undefined)
-    }else if(body.message){
-        callback("Unable to find location, try something valid.",undefined)
-    }else{
-        callback(undefined,"It is currently "+body.daily[0].weather[0].description+" with a temp of "+body.current.temp+" degree celsius out. There is a humidity of "+body.current.humidity+ "%")
-    }
-})
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=38e899ebd737adbc97156a0b1848ba5f&units=metric'
+    request({ url, json: true }, (error, { body } = response) => {
+        if (error) {
+            callback("Not connected to the web server.Check internet connection.", undefined)
+        } else if (body.message) {
+            callback("Unable to find location, try something valid.", undefined)
+        } else {
+            callback(undefined, "It is currently having a weather of " + body.daily[0].weather[0].description + " with a temp of " +
+             body.current.temp + " degree celsius. There is a humidity of " + body.current.humidity + "%." + 
+             " The average temperature of the day is " + body.daily[0].temp.day + " dergree celsius. The minimum temperature of the day is "+
+             body.daily[0].temp.min+ " degree celsius. The maximum temperature of the day is "+
+             body.daily[0].temp.max + " degree celsius."+" Clouds percentage "+body.current.clouds+" %.")
+        }
+    })
 }
 
 module.exports = forecast
